188d23429837ea406b18e33b9be5f6ff4c9f0f59
Dylan Armstrong <dylan.armstrong@dylanarmstrong.net>
2023-01-31 16:35:13 +1100

    Added pretty messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,13 @@
 function Message(props) {
     return (
         <div className="chat">
-            <div className="message-sender">
-                <p><strong>{props.sender}</strong></p>
-            </div>
-            <div className="message-body">
-                <p>{props.message}</p>
+            <div class="card">
+                <div class="card-header">
+                    <strong>{props.sender}</strong>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">{props.message}</p>
+                </div>
             </div>
         </div>
     );   
